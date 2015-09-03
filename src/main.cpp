901ef2d0f25c2d1f7d09ed3c89f50f033c4e25be
jasonandrews271192@gmail.com <jasonandrews271192@gmail.com>
2015-09-03 22:23:31 +0100

    Started project - added src/main.cpp.

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 3 Sep 2015
+ *      Author: Jason
+ */
+
+#include <iostream>
+
+int main() {
+
+	std::cout << "Application is running.." << std::endl;
+
+	return 0;
+}
+
+
