92a3dbc0c8038140b496cb1b8aace420e7e13c53
jasonandrews271192@gmail.com <jasonandrews271192@gmail.com>
2015-09-04 23:03:38 +0100

    Added GameManager.hpp & GameManager.cpp.

---
+++ src/GameManager.hpp
@@ -0,0 +1,20 @@
+/*
+ * GameManager.hpp
+ *
+ *  Created on: 4 Sep 2015
+ *      Author: Jason
+ */
+
+#ifndef GAMEMANAGER_HPP_
+#define GAMEMANAGER_HPP_
+
+class GameManager {
+
+public:
+	GameManager();
+
+};
+
+
+
+#endif /* GAMEMANAGER_HPP_ */
---
+++ src/GameManger.cpp
@@ -0,0 +1,13 @@
+/*
+ * GameManger.cpp
+ *
+ *  Created on: 4 Sep 2015
+ *      Author: Jason
+ */
+
+#include "GameManager.hpp"
+
+GameManager::GameManager() {
+
+}
+
